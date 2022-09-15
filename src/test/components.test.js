df8425b786abc1153c7a27ac4fb3e68f453d4e1a
Christabelle143 <christabelleyouta143@gmail.com>
2022-09-15 20:56:01 +0100

    write test cases to test the react components

---
+++ src/test/components.test.js
@@ -0,0 +1,33 @@
+/* eslint-disable import/extensions */
+import React from 'react';
+import renderer from 'react-test-renderer';
+// import { Router } from 'react-router-dom';
+import Calculator from './components/Calculator';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Quote from './components/Quote';
+
+it('should render home component correctly', () => {
+  const home = renderer
+    .create(<Home />)
+    .toJSON();
+  expect(home).toMatchSnapshot();
+});
+it('should render quote component correctly', () => {
+  const quote = renderer
+    .create(<Quote />)
+    .toJSON();
+  expect(quote).toMatchSnapshot();
+});
+it('should render Calculator component correctly', () => {
+  const calculate = renderer
+    .create(<Calculator />)
+    .toJSON();
+  expect(calculate).toMatchSnapshot();
+});
+it('should render Navbar component correctly', () => {
+  const navbar = renderer
+    .create(<Navbar />)
+    .toJSON();
+  expect(navbar).toMatchSnapshot();
+});
